72108b7ea24fde972474d4816fffc7f0f941a41b
hussain-debug <developershussain@gmail.com>
2023-05-03 20:27:33 +0530

    final

--- main.js
+++ main.js
@@ -3,103 +3,33 @@ var chatbotBtn = document.getElementById("chatbot-btn");
 
 function chtbt(){
     showContactForm();
+    chatbotBtn.style.display="none"
 
 }
 
 function showContactForm() {
     var contactForm = document.createElement("div");
+
+    var form=document.getElementById("form")
     contactForm.id = "contact-form";
-    var existingForm = document.getElementById("contact-form");
-    if (existingForm) {
-      return;
-    }
+    // var existingForm = document.getElementById("contact-form");
+    // if (existingForm) {
+    //   return;
+    // }
     contactForm.style.position = "fixed";
   contactForm.style.bottom = "0";
   contactForm.style.right = "0";
     contactForm.style.width = "400px";
-    contactForm.style.height = "340px";
+    contactForm.style.height = "355px";
     contactForm.style.borderRadius="20px";
     contactForm.style.backgroundColor = "#fff";
     contactForm.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.3)";
     contactForm.style.transition = "0.5s";
 
+form.style.display="block";
+form.style.padding="10px";
 
-
-
-    var form = document.createElement("form");
-    form.style.padding="10px";
-
-    form.setAttribute('name', 'contact');
-
-    // Set the form method attribute
-    form.setAttribute('method', 'POST');
-    
-    // Set the data-netlify attribute
-    form.setAttribute('data-netlify', 'true');
-    
-    form.addEventListener('submit', function(e) {
-      e.preventDefault(); // prevent the form from submitting normally
-    });    
-
-
-    var fieldset = document.createElement("fieldset");
-    fieldset.className = "form-group";
-    
-    var nameLabel = document.createElement("label");
-    nameLabel.for = "name";
-    nameLabel.innerText = "Name";
-    var nameInput = document.createElement("input");
-    nameInput.type = "text";
-    nameInput.className = "form-control";
-    nameInput.id = "name";
-    
-    var emailLabel = document.createElement("label");
-    emailLabel.for = "email";
-    emailLabel.innerText = "Email";
-    var emailInput = document.createElement("input");
-    emailInput.type = "email";
-    emailInput.className = "form-control";
-    emailInput.id = "email";
-    
-    var messageLabel = document.createElement("label");
-    messageLabel.for = "message";
-    messageLabel.innerText = "Message";
-    var messageTextarea = document.createElement("textarea");
-    messageTextarea.className = "form-control";
-    messageTextarea.id = "message";
-    messageTextarea.rows = "3";
-    
-    var submitButton = document.createElement("button");
-    submitButton.type = "submit";
-    submitButton.className = "btn btn-info";
-    submitButton.innerText = "send";
-    submitButton.style.border = "none";
-    submitButton.style.color = "white";
-    submitButton.style.position = "relative";
-    submitButton.style.top = "20px";
-
-    submitButton.style.backgroundColor = "#17A2B8;";
-
-
-    // submitButton.addEventListener("mouseover", function() {
-    //   submitButton.style.backgroundColor = " #17A2B8;";
-    //   submitButton.style.borderColor = "none";
-    // });
-
-    // submitButton.addEventListener("mouseout", function() {
-    //   submitButton.style.backgroundColor = " #17A2B8;";
-    //   submitButton.style.borderColor = "none";
-    // });
-    
-    fieldset.appendChild(nameLabel);
-    fieldset.appendChild(nameInput);
-    fieldset.appendChild(emailLabel);
-    fieldset.appendChild(emailInput);
-    fieldset.appendChild(messageLabel);
-    fieldset.appendChild(messageTextarea);
-    fieldset.appendChild(submitButton);
-    
-    form.appendChild(fieldset);
+   
     contactForm.appendChild(form);
 
     var closeButton = document.createElement("button");
@@ -112,9 +42,12 @@ function showContactForm() {
     closeButton.style.backgroundColor = "transparent";
     closeButton.style.cursor = "pointer";
 closeButton.addEventListener("click", function() {
-  document.body.removeChild(contactForm);
-});
+  contactForm.style.display="none";
+  chatbotBtn.style.display="block"
+  
 
+});
+ 
  var closeIcon = document.createElement("i");
   closeIcon.className = "fa fa-times";
 
